55ca79c13bb76a0d426f0148ee8aec272400d5cd
sjamescarter <stephenjamescarter@gmail.com>
2023-04-04 17:30:18 -0500

    commit tree b75177a15dad6d44e2a009362b634de64aa44d12

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+('Mr. Chocolate', 20, 'M', 'Brown', 'Passive', 1),
+('Rowdy', 12, 'M', 'Black', 'Rowdy', 1),
+('Tabitha', 5, 'F', 'Black', 'Playful', 1),
+('Sergeant Brown', 31, 'M', 'Brown', 'Aggressive', 0),
+('Melissa', 18, 'F', 'White', 'Dominant', 1),
+('Grinch', 52, 'M', 'Green', 'Grumpy', 0),
+('Wendy', 2, 'F', 'Black', 'Amicable', 1),
+(NULL, NULL, 'F', 'Brown', 'Moody', 1);
